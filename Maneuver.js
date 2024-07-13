f72592af60707520d6a19e98778747075eb25584
seanmd8 <smd8@pdx.edu>
2024-07-13 01:13:01 -0700

    added roar_of_challenge prereq

--- Maneuver.js
+++ Maneuver.js
@@ -9179,8 +9179,13 @@ function roar_of_challenge(){
         name: boon_names.roar_of_challenge,
         pic: `${IMG_FOLDER.boons}roar_of_challenge.png`,
         description: roar_of_challenge_description,
+        prereq: prereq_roar_of_challenge,
         on_pick: pick_roar_of_challenge,
     }
+}
+
+function prereq_roar_of_challenge(){
+    return GS.map.get_player().max_health !== undefined;
 }
 
 function pick_roar_of_challenge(){
