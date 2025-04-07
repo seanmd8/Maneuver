0a8b2d3cefe26603c3418638f0c26785491a0fba
seanmd8 <smd8@pdx.edu>
2025-04-07 15:47:32 -0700

    Updated ReadMe for Arcane Library update

--- Maneuver.js
+++ Maneuver.js
@@ -1479,7 +1479,7 @@ const basement_description = `You have entered the basement.`;
 const magma_description = `You have entered the magmatic caves.`;
 const crypt_description = `You have entered the crypt.`;
 const forest_description = `You have entered the subteranean forest.`;
-const library_description = `You have entered the library.`;
+const library_description = `You have entered the arcane library.`;
 const sanctum_description = `You have entered the sanctum.`;
 const default_area_description = `You have reached the end of the current content. Floors will continue to generate but there will `
                                 +`be no more boss fights. Good luck.`;
