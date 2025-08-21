222c5edad790646fbf80a44fcda3c8e47ba40019
seanmd8 <smd8@pdx.edu>
2025-08-21 13:13:00 -0700

    Moved Shop Images to their own file.

--- Maneuver.js
+++ Maneuver.js
@@ -14120,8 +14120,6 @@ function teleport(){
         options
     }
 }
-// ----------------ShopImages.js----------------
-// File containing cards used soley to display images in card rows of the shop.
 
 /** @type {CardGenerator} Shown in shop to denote adding a card to your deck.*/
 function add_card_symbol(){
@@ -14131,19 +14129,21 @@ function add_card_symbol(){
         options: new ButtonGrid()
     }
 }
-/** @type {CardGenerator} Shown in show to denote removing a card from your deck.*/
-function remove_card_symbol(){
-    return{
-        name: `Remove`,
-        pic: `${IMG_FOLDER.other}minus.png`,
-        options: new ButtonGrid()
-    }
-}
+
 /** @type {CardGenerator} Shown in shop ind=stead of the remove symbol when your deck is at the minimum size.*/
 function deck_at_minimum_symbol(){
     return{
         name: `Minimum`,
         pic: `${IMG_FOLDER.other}x.png`,
+        options: new ButtonGrid()
+    }
+}
+
+/** @type {CardGenerator} Shown in show to denote removing a card from your deck.*/
+function remove_card_symbol(){
+    return{
+        name: `Remove`,
+        pic: `${IMG_FOLDER.other}minus.png`,
         options: new ButtonGrid()
     }
 }
