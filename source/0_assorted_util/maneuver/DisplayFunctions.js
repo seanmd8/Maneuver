daebefbf25d24c2c86c973883c5b60b66435aa50
seanmd8 <smd8@pdx.edu>
2025-08-28 12:53:00 -0700

    Organized css files.

--- source/0_assorted_util/maneuver/DisplayFunctions.js
+++ source/0_assorted_util/maneuver/DisplayFunctions.js
@@ -78,7 +78,7 @@ function telegraph_repetition_boon(repeat){
 function display_entire_deck(deck){
     // Display section header.
     var min_deck_size = deck.deck_min();
-    display.display_message(UIIDS.current_deck, `${current_deck}${min_deck_size}):`);
+    display.display_message(UIIDS.current_deck, `${shop_text.current}${min_deck_size}):`);
     // Display deck with limited cards per line.
     var decklist = deck.get_deck_info();
     var card_explanation = (card) => {
