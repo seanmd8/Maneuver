2ec6d4ce9b52457d36938697d2731e04d38f623d
seanmd8 <smd8@pdx.edu>
2025-09-28 15:46:52 -0700

    Buffed card: Diamond Attack

--- Maneuver.js
+++ Maneuver.js
@@ -15379,8 +15379,8 @@ function dash_nw(){
 function diamond_attack(){
     var options = new ButtonGrid();
     options.add_button(SPIN, [pattack(0, -1), pattack(1, 0), pattack(0, 1), pattack(-1, 0)]);
-    options.add_button(SE, [pmove(1, 1)]);
-    options.add_button(SW, [pmove(-1, 1)]);
+    options.add_button(SE, [pmove(2, 1)]);
+    options.add_button(SW, [pmove(-2, 1)]);
     return{
         name: card_names.diamond_attack,
         pic: `${IMG_FOLDER.cards}diamond_attack.png`,
--- source/6_move_cards/common_cards/diamond_attack.js
+++ source/6_move_cards/common_cards/diamond_attack.js
@@ -2,8 +2,8 @@
 function diamond_attack(){
     var options = new ButtonGrid();
     options.add_button(SPIN, [pattack(0, -1), pattack(1, 0), pattack(0, 1), pattack(-1, 0)]);
-    options.add_button(SE, [pmove(1, 1)]);
-    options.add_button(SW, [pmove(-1, 1)]);
+    options.add_button(SE, [pmove(2, 1)]);
+    options.add_button(SW, [pmove(-2, 1)]);
     return{
         name: card_names.diamond_attack,
         pic: `${IMG_FOLDER.cards}diamond_attack.png`,
