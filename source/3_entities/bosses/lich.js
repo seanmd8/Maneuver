75a1058cc14df9ff9dbf1f623cb3427d83d336a7
seanmd8 <smd8@pdx.edu>
2025-10-10 15:55:47 -0700

    Fixed bug where lich announced upon journal entry.

--- source/3_entities/bosses/lich.js
+++ source/3_entities/bosses/lich.js
@@ -54,16 +54,16 @@ function lich_ai(self, target, map){
         var moved = move_careful(self, target, map, directions);
         if(moved === undefined){
             // If stuck, prep teleport.
-            lich_prep(self.tile, -1);
+            say_record(lich_prep(self.tile, -1));
         }
         else{
-            lich_prep(self.tile, random_num(self.tile.spells.length));
+            say_record(lich_prep(self.tile, random_num(self.tile.spells.length)));
         }
     }
     else if(self.tile.cycle === -1){
         // Cast teleport.
         LICH_UTIL_SPELLS[1].behavior(self, target, map);
-        lich_prep(self.tile, -2);
+        say_record(lich_prep(self.tile, -2));
     }
     else{
         // Cast the current spell.
@@ -72,7 +72,7 @@ function lich_ai(self, target, map){
         if(self.tile.spells.length === 0){
             self.tile.spells = [...LICH_SPELLS];
         }
-        lich_prep(self.tile, -2);
+        say_record(lich_prep(self.tile, -2));
     }
 }
 
@@ -115,7 +115,7 @@ function lich_hit(self, target, map){
                 self.tile.spells = [...LICH_SPELLS];
             }
         }
-        lich_prep(self.tile, -1, true);
+        say_record(lich_prep(self.tile, -1, true));
     }
 }
 
@@ -133,5 +133,5 @@ function lich_prep(tile, cycle, change = false){
             `${boss_descriptions.lich_change_announcement}\n` : 
             `${boss_descriptions.lich_announcement}\n`)
         +`${spell.description}`;
-    say_record(announcement);
+    return announcement;
 }
