942a38ddfe81509f247d138fd6dd837e898f4e77
seanmd8 <smd8@pdx.edu>
2025-10-11 13:33:39 -0700

    Added port bash script to make a zipped version of the directory with only necessary files which can be uploaded to itch.io.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 python py/staple.py build
 start chrome --new-window $(PWD)/index.html
 # Run using ./build.sh in a bash terminal
---
+++ port.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zip -r -u Maneuver.zip fonts/ images/ index.html Maneuver.js ManeuverStyles.css README.md
