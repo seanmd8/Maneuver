393fc7b3d204b8f79ed684ab25d05def2446fb4f
seanmd8 <smd8@pdx.edu>
2026-03-27 15:47:19 -0700

    Split up achievement text into multiple files.

--- Maneuver.js
+++ Maneuver.js
@@ -916,10 +916,10 @@ const boon_messages = {
     no_max: `Unlimited`,
     number_picked: `Times Picked`,
 
-    burn_bright: `Your maximum health has decreased.`,
+    burn_bright: `Your max health has decreased.`,
     clean_mind: [`Choose a card to remove (`, `/2 remaining)`],
     duplicate: `Choose a card to copy:`,
-    practice_makes_perfect: `Your maximum health has increased.`,
+    practice_makes_perfect: `Your max health has increased.`,
     rebirth: `You died, but were brought back to life.`,
     soul_voucher: `Your voucher will negate the cost.`
 }
@@ -2147,52 +2147,6 @@ const special_tile_names = {
     player: `Player`,
 }
 Object.freeze(special_tile_names);
-const achievement_text = {
-    title: `Achievements`,
-    reset: `Reset`,
-    confirm_reset: `Confirm?`,
-    unlocked: `Achievement Unlocked:`,
-    repeated: `Achievement Repeated:`,
-    unlocks_boon: `New Boon`,
-    unlocks_cards: `New Cards`,
-}
-Object.freeze(achievement_text);
-
-const achievement_names = {
-    // Boss
-    velociphile: `Only A Speedbump`,
-    spider_queen: `Arachno-Regicide`,
-    two_headed_serpent: `One Head Is Better Than Two`,
-    lich: `End To Unlife`,
-    young_dragon: `Novice Dragonslayer`,
-    forest_heart: `Expert Lumberjack`,
-    arcane_sentry: `Security Bypass`,
-    lord_of_shadow_and_flame: `Deeper and Deeper`,
-    victory: `Victory`,
-
-    // Normal
-    ancient_knowledge: `Ancient Knowledge`,
-    beyond_the_basics: `Beyond The Basics`,
-    blessed: `Blessed`,
-    clumsy: `Clumsy`,
-    collector: `Collector`,
-    common_sense: `Common Sense`,
-    jack_of_all_trades: `Jack Of All Trades`,
-    manic_vandal: `Manic Vandal`,
-    minimalist: `Minimalist`,
-    monster_hunter: `Monster Hunter`,
-    non_violent: `Non Violent`,
-    not_my_fault: `Not My Fault`,
-    one_hit_wonder: `One Hit Wonder`,
-    one_life: `One Is All You Need`,
-    peerless_sprinter: `Peerless Sprinter`,
-    shrug_it_off: `Shrug It Off`,
-    speed_runner: `Speed Runner`,
-    triple: `Three Of A Kind`,
-    without_a_scratch: `Without A Scratch`,
-}
-Object.freeze(achievement_names);
-
 const achievement_description = {
     // Boss
     velociphile: `Defeat the boss of the ${area_names.ruins}.`,
@@ -2227,6 +2181,49 @@ const achievement_description = {
     without_a_scratch: `Leave floor 10 without ever taking damage.`,
 }
 Object.freeze(achievement_description);
+const achievement_names = {
+    // Boss
+    velociphile: `Only A Speedbump`,
+    spider_queen: `Arachno-Regicide`,
+    two_headed_serpent: `One Head Is Better Than Two`,
+    lich: `End To Unlife`,
+    young_dragon: `Novice Dragonslayer`,
+    forest_heart: `Expert Lumberjack`,
+    arcane_sentry: `Security Bypass`,
+    lord_of_shadow_and_flame: `Deeper and Deeper`,
+    victory: `Victory`,
+
+    // Normal
+    ancient_knowledge: `Ancient Knowledge`,
+    beyond_the_basics: `Beyond The Basics`,
+    blessed: `Blessed`,
+    clumsy: `Clumsy`,
+    collector: `Collector`,
+    common_sense: `Common Sense`,
+    jack_of_all_trades: `Jack Of All Trades`,
+    manic_vandal: `Manic Vandal`,
+    minimalist: `Minimalist`,
+    monster_hunter: `Monster Hunter`,
+    non_violent: `Non Violent`,
+    not_my_fault: `Not My Fault`,
+    one_hit_wonder: `One Hit Wonder`,
+    one_life: `One Is All You Need`,
+    peerless_sprinter: `Peerless Sprinter`,
+    shrug_it_off: `Shrug It Off`,
+    speed_runner: `Speed Runner`,
+    triple: `Three Of A Kind`,
+    without_a_scratch: `Without A Scratch`,
+}
+Object.freeze(achievement_names);
+const achievement_text = { title: `Achievements`,
+    reset: `Reset`,
+    confirm_reset: `Confirm?`,
+    unlocked: `Achievement Unlocked:`,
+    repeated: `Achievement Repeated:`,
+    unlocks_boon: `New Boon`,
+    unlocks_cards: `New Cards`,
+}
+Object.freeze(achievement_text);
 
 const boss_achievements = [
     achievement_names.velociphile,
@@ -2237,7 +2234,7 @@ const boss_achievements = [
     achievement_names.forest_heart,
     achievement_names.arcane_sentry,
     achievement_names.lord_of_shadow_and_flame,
-]
+];
 const stat_image_labels = {
     deck: `Cards in deck`,
     floor: `Floor number`,
--- source/1_gametext/descriptions/screens/achievements/achievement_descriptions.js
+++ source/1_gametext/descriptions/screens/achievements/achievement_descriptions.js
@@ -1,49 +1,3 @@
-const achievement_text = {
-    title: `Achievements`,
-    reset: `Reset`,
-    confirm_reset: `Confirm?`,
-    unlocked: `Achievement Unlocked:`,
-    repeated: `Achievement Repeated:`,
-    unlocks_boon: `New Boon`,
-    unlocks_cards: `New Cards`,
-}
-Object.freeze(achievement_text);
-
-const achievement_names = {
-    // Boss
-    velociphile: `Only A Speedbump`,
-    spider_queen: `Arachno-Regicide`,
-    two_headed_serpent: `One Head Is Better Than Two`,
-    lich: `End To Unlife`,
-    young_dragon: `Novice Dragonslayer`,
-    forest_heart: `Expert Lumberjack`,
-    arcane_sentry: `Security Bypass`,
-    lord_of_shadow_and_flame: `Deeper and Deeper`,
-    victory: `Victory`,
-
-    // Normal
-    ancient_knowledge: `Ancient Knowledge`,
-    beyond_the_basics: `Beyond The Basics`,
-    blessed: `Blessed`,
-    clumsy: `Clumsy`,
-    collector: `Collector`,
-    common_sense: `Common Sense`,
-    jack_of_all_trades: `Jack Of All Trades`,
-    manic_vandal: `Manic Vandal`,
-    minimalist: `Minimalist`,
-    monster_hunter: `Monster Hunter`,
-    non_violent: `Non Violent`,
-    not_my_fault: `Not My Fault`,
-    one_hit_wonder: `One Hit Wonder`,
-    one_life: `One Is All You Need`,
-    peerless_sprinter: `Peerless Sprinter`,
-    shrug_it_off: `Shrug It Off`,
-    speed_runner: `Speed Runner`,
-    triple: `Three Of A Kind`,
-    without_a_scratch: `Without A Scratch`,
-}
-Object.freeze(achievement_names);
-
 const achievement_description = {
     // Boss
     velociphile: `Defeat the boss of the ${area_names.ruins}.`,
@@ -78,14 +32,3 @@ const achievement_description = {
     without_a_scratch: `Leave floor 10 without ever taking damage.`,
 }
 Object.freeze(achievement_description);
-
-const boss_achievements = [
-    achievement_names.velociphile,
-    achievement_names.spider_queen,
-    achievement_names.two_headed_serpent,
-    achievement_names.lich,
-    achievement_names.young_dragon,
-    achievement_names.forest_heart,
-    achievement_names.arcane_sentry,
-    achievement_names.lord_of_shadow_and_flame,
-]
